c71e9849289ac78ebab32f01d12c4ae1d78e7427
Lenastin <Lenastin@users.noreply.github.com>
2018-04-10 16:39:36 -0800

    added units to input

--- Lab8.py
+++ Lab8.py
@@ -26,10 +26,10 @@ def calcLeverArm2(centerOfSystem, centerOfStick):
         leverArmLen2 = centerOfStick - centerOfSystem
     return leverArmLen2
 
-centerOfStick = float(input("Where is the center of mass of the meter stick?: "))
-addedMassLocation = float(input("Where did you put the added mass at?: "))
-centerOfSystem = float(input("Where is the center of mass of the system?: "))
-addedMass = float(input("How much mass did you add?: "))
+centerOfStick = float(input("Where is the center of mass of the meter stick? (m): "))
+addedMassLocation = float(input("Where did you put the added mass at? (m): "))
+centerOfSystem = float(input("Where is the center of mass of the system? (m): "))
+addedMass = float(input("How much mass did you add? (kg): "))
 leverArmLen1 = calcLeverArm1(centerOfSystem, addedMassLocation)
 leverArmLen2 = calcLeverArm2(centerOfSystem, centerOfStick)
 massOfStick = calcMass(addedMass, leverArmLen1, leverArmLen2)
