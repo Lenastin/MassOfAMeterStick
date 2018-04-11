acfc51e2cb7a6cfd7af054c72dcf26b10f9b567b
Lenastin <Lenastin@users.noreply.github.com>
2018-04-10 16:37:13 -0800

    basic meter stick mass finder

--- Lab8.py
+++ Lab8.py
@@ -13,10 +13,24 @@ def calcMass(addedMass, leverArmLen1, leverArmLen2):
     force = calcForce(torqueOfMass, leverArmLen2)
     mass = force / 9.82
     return mass
+def calcLeverArm1(centerOfSystem, massLocation):
+    if massLocation < centerOfSystem:
+        leverArmLen1 = centerOfSystem - massLocation
+    else:
+        leverArmLen1 = massLocation - centerOfSystem
+    return leverArmLen1
+def calcLeverArm2(centerOfSystem, centerOfStick):
+    if centerOfStick < centerOfSystem:
+        leverArmLen2 = centerOfSystem - centerOfStick
+    else:
+        leverArmLen2 = centerOfStick - centerOfSystem
+    return leverArmLen2
 
+centerOfStick = float(input("Where is the center of mass of the meter stick?: "))
+addedMassLocation = float(input("Where did you put the added mass at?: "))
+centerOfSystem = float(input("Where is the center of mass of the system?: "))
 addedMass = float(input("How much mass did you add?: "))
-leverArmLen1 = float(input("How far from the center of mass of the system is the added mass?: "))
-leverArmLen2 = float(input("How far away is the center of mass of the meter stick from the center of mass of the system?: "))
-
+leverArmLen1 = calcLeverArm1(centerOfSystem, addedMassLocation)
+leverArmLen2 = calcLeverArm2(centerOfSystem, centerOfStick)
 massOfStick = calcMass(addedMass, leverArmLen1, leverArmLen2)
-print(massOfStick)
+print(massOfStick, "kg")
